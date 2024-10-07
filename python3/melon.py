5231876047c4689da6c0b4147a4696c3ec98bfa6
CSJ 9013 <foreducation0001@hotmail.com>
2024-10-07 01:44:38 +0000

    멜론 크롤링 수정

---
+++ python3/melon.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 웹 접속
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get("https://www.melon.com/", headers=headers)
+print(data) # 접속이 잘 됐는 지 확인
+
+# html 정보 가져오기
+soup = BeautifulSoup(data.text, 'html.parser')
+ 
+# 가져온 html 정보 출력 / 주석처리 이유는 밑에 타이틀을 출력 중이기에
+# print(soup)
+
+# 순위 출력
+for i in range(1,11) :
+    title = soup.select_one(f'#conts > div.chart > div > ul > li.on.nth1 > div > ul > li:nth-child({i}) > div.rank_cntt > div.rank_info > p > a')
+    print(f'{i}위 {title.text}')
+
+# 멜론 차트 1위 제목 크롤링
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.melon.com/chart/',headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+title = soup.select_one('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a ')
+
+# 노래 제목
+#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a
+# 가수 이름
+#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank02 > a
+
+print(title)
+
+# 제목만 가져오기
+print(title.text)
+
+top_100 = soup.select('#lst50 > td > div')
+
+for tr in top_100 :
+    rank = tr.select_one('span.rank')
+        
+    if rank is not None:
+        print(rank.text, end=". ")
+        
+    title = tr.select_one('div > div.ellipsis.rank01 > span > a')
+    
+    if title is not None:
+        artist = tr.select_one('div.ellipsis.rank02 > a').text
+        print(artist,'-', title.text)
--- python3/randtext.py
+++ python3/randtext.py
@@ -5,28 +5,3 @@ import random
 print(random.random())
 menu = "치킨", "피자", "김밥", "탕수육"
 print(random.choice(menu))
-
-import requests
-from bs4 import BeautifulSoup
-
-# 웹 접속
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get("https://www.melon.com/", headers=headers)
-print(data) # 접속이 잘 됐는 지 확인
-
-# html 정보 가져오기
-soup = BeautifulSoup(data.text, 'html.parser')
- 
-# 가져온 html 정보 출력 / 주석처리 이유는 밑에 타이틀을 출력 중이기에
-# print(soup)
-
-#멜론 차트 1위 제목 크롤링
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.melon.com/chart/',headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
-title = soup.select_one('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a ')
-
-print(title)
-
-# 제목만 가져오기
-print(title.text)
