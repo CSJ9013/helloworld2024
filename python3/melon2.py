d00783c7b61486fc2e117153a9493293f3ebb573
CSJ 9013 <foreducation0001@hotmail.com>
2024-10-14 01:43:17 +0000

    테스트 파일 추가 & melon2 주석 추가

--- python3/melon2.py
+++ python3/melon2.py
@@ -14,22 +14,23 @@ soup = BeautifulSoup(data.text, 'html.parser')
 # for i in range(0,50) :
     # print(f'{i+1}위 {title[i].text} - {artist[i].text}')
 
-# 리스트 변수
+# 리스트 변수, rank = 1~100 까지를 담고 있는 리스트 (데이터)
 rank = []
 for i in range(1,101):
     rank.append(i)
 print(rank)
-# 제목 리스트 만들기
+# 제목 리스트 (데이터) 만들기
 tlist = []
 title = soup.select('div.ellipsis.rank01 > span > a')
 for t in title:
     tlist.append(t.text)
-# 가수 리스트 만들기
+# 가수 리스트 (데이터) 만들기
 alist = []
 artist = soup.select('div.ellipsis.rank02 > span > a:nth-child(1)')
 for a in artist:
     alist.append(a.text)
 
+# 키 & 리스트 생성 (순위, 제목, 가수 = 키) (rank, tlist, alist = 리스트)
 df3 = pd.DataFrame({'순위': rank,
                     '제목': tlist,
                     '가수': alist})
