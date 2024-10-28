31a048c146443699f544c0dca6964b5cb0acbcf5
CSJ 9013 <foreducation0001@hotmail.com>
2024-10-28 02:38:15 +0000

    플라스크 응용

--- python3/web/app.py
+++ python3/web/app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/baseball')
+# def = 파이썬 함수, 아무거나 작성 가능 그냥 안겹치는 걸로
+def baseball():
+    return 'Hello, baseball!'
+
+@app.route('/search')
+def searchhtml():
+    return render_template("search.html")
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        return "POST로 전달"
+
 if __name__ == '__main__':
     app.run()
