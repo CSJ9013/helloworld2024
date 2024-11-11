098fadf788f0a7a1fb102be2263f031e33b26963
CSJ 9013 <foreducation0001@hotmail.com>
2024-11-11 02:49:11 +0000

    api 활용 및 함수 학습

---
+++ python3/web/test.py
@@ -0,0 +1,14 @@
+#갑분 함수 배우기 / 밑엔 함수
+def hello(msg) :
+    ## print("hello world")
+    ## 밑처럼 해야지 "hello 검색어" 출력, 위처럼 쓰면 hello world 출력
+    #print(f"hello {msg}")
+    # return "출력" # 결과값(있어도 없어도 괜찮음, 출력이니)
+    return f"hello {msg}"
+
+# 함수 부르기
+## hello("검색어")
+keyword = input("맛집 검색 : ") # 입력창에 입력 받기
+# 입력 받은 값을 keyword에 저장, 입력 받은 데이터를 hello 함수에 입력값으로 넣기
+result = hello(keyword) # 위 함수에서 전달된 값을 result에 전달
+print(result) # 전달된 값 화면에 출력
---
+++ python3/web/test2.py
@@ -0,0 +1,5 @@
+# 네이버 검색 활용
+import nvapi
+
+keyword = input("검색 : ")
+nvapi.blog(keyword)
